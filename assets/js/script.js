4639afdd1878ad6765b5f894d71302e9e7a5ced0
Fleepx <felipesanchez.aq@gmail.com>
2024-06-08 17:08:16 -0400

    correcionCart

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-var cartCount = document.querySelector('#cart-count'); //Etiqueta para Contador
-var montoTotal = document.querySelector('#totalPagar'); //Etiqueeta para Monto Total
+var cartCount = document.querySelector('#cart-count'); // Etiqueta para Contador
+var montoTotal = document.querySelector('#totalPagar'); // Etiqueeta para Monto Total
 let cantidad = 0;
 let totalPagar = 0;
 
-function incrementar(precio) { //Formula para incrementar la cantidad y el precio
+function incrementar(precio) { // Formula para incrementar la cantidad y el precio
     if (cantidad < 99) {
         cantidad++;
         cartCount.textContent = cantidad;
@@ -11,7 +11,7 @@ function incrementar(precio) { //Formula para incrementar la cantidad y el preci
         montoTotal.textContent = totalPagar;
     }
 }
-function disminuir(precio) { //Formula para disminuir la cantidad y el precio
+function disminuir(precio) { // Formula para disminuir la cantidad y el precio
     if (cantidad > 0) {
         cantidad--;
         cartCount.textContent = cantidad;
