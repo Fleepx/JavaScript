c2de4a09be0c02578793c34b6112e0ecb741ecfc
Fleepx <felipesanchez.aq@gmail.com>
2024-06-07 23:21:23 -0400

    Avance

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var cartCount = document.querySelector('#cart-count');
+let cantidad = 0;
+let totalPagar = 0;
+
+document.querySelector(".precio").innerHTML = precioBase;
+document.querySelector(".cantidad").innerHTML = cantidad;
+document.querySelector("#totalPagar").innerHTML = totalPagar;
+
